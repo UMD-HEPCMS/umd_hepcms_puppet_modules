803594ee4fa64b83727c61b92ae7cbac1ae9059f
mtonjes <mtonjes@umd.edu>
2015-08-11 14:18:38 -0400

    oh, these were already in base.pp, removing

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,3 @@
 # site.pp
 
 #hiera_include('classes')
-# added 11 Aug 2015 per instructions for iptables: https://github.com/treydock/puppet-iptables
-Firewall {
-  before  => Class['iptables::post'],
-  require => Class['iptables::pre'],
-}
