4e6fcd58804da9a0bd9ebae0aee4024c71b665b4
mtonjes <mtonjes@umd.edu>
2015-08-29 19:21:16 -0400

    go back to empty site.pp
    Sites other than this one got very cranky.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,21 +1,3 @@
 # site.pp
 
 #hiera_include('classes')
-node 'hepcms-in2.umd.edu' {
-# make symlink test on one site
-# http://www.puppetcookbook.com/posts/creating-a-directory.html
-# create a directory      
-#file { "/etc/site-conf":
-#    ensure => "directory",
-#}
-# ln -s /data/hadoop /hadoop
-# ln -s /data/hadoop/store /store 
-file { '/hadoop':
-  ensure => 'symlink',
-  target => '/data/hadoop',
-}
-file { '/store':
-  ensure => 'symlink',
-  target => '/data/hadoop/store',
-}
-}
