7f29995be7e856fe6f442f644e585cf5b3bc903e
mtonjes <mtonjes@umd.edu>
2015-11-12 13:00:44 -0500

    need a default node
    test is done anyway

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,9 @@
 # site.pp
 
 #hiera_include('classes')
-#Use to test puppet changes on a single node
-node 'hepcms-in1.umd.edu' {
-class { 'yum_cron':
-  yum_autoupdate_ensure => 'absent'
-}
-}
+#Use to test puppet changes on a single node (note that the other nodes complain there's no default :(
+#node 'hepcms-in1.umd.edu' {
+#class { 'yum_cron':
+#  yum_autoupdate_ensure => 'absent'
+#}
+#}
