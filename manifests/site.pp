535ba05e6cebc2a685c40a8791343ff8da0a53f9
mtonjes <mtonjes@umd.edu>
2015-12-18 12:02:59 -0500

    inherits default
    So that everyone has classes from hiera without specifying it below.

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,11 +6,11 @@
 # default node MUST exist (do all the rest of the default type things in base.pp, note that 
 # hepcms-hn and hepcms-foreman are NOT part of base.pp, and that's ok)
 # need this classes line in every node to be able to use classes in hiera
-# hiera_include('classes',[])
+# hiera_include('classes',[]) (not sure if ,[] is needed, never tested, works without it right now 18 Dec 2015 MBT)
 node default {
 hiera_include('classes')
 }
-### you MUST put hiera_include('classes') in your individual nodes if you want it to use hiera classes
+### the inherits default takes care of making sure classes are there for all nodes specified below
 # example implementation:
 # note that .privnet not needed, but .umd.edu might be for public IP nodes
 #node 'FQDN-As-Foreman-KnowsIt'{
@@ -19,13 +19,13 @@ hiera_include('classes')
 #     target => '/data/hadoop',
 #   }
 #}
-node 'hepcms-vmtest'{
+node 'hepcms-vmtest' inherits default{
 # include ::profile::osg::hadoop_client
 }
-node 'foreman-vmtest2'{
-include ::profile::osg::hadoop_client
-}
-node 'hepcms-in1'{
+node 'foreman-vmtest2' inherits default{
 # include ::profile::osg::hadoop_client
-hiera_include('classes',[])
 }
+# node 'hepcms-in1' inherits default{
+# # include ::profile::osg::hadoop_client
+# hiera_include('classes',[])
+# }
